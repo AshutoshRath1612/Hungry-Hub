a2f8ac262b464eb63dfdeb325e76e51e3c774801
AshutoshRath1612 <ashutosh.rath1613@ibm.com>
2024-03-17 00:58:42 +0530

    initial commit

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,16 +1,22 @@
-import {Button, Image, StyleSheet, Text, View , TouchableOpacity } from 'react-native'
+import {Button, Image, StyleSheet, Text, View , TouchableOpacity, ImageBackground } from 'react-native'
 import React, { Component } from 'react'
 import {LinearGradient} from 'expo-linear-gradient';
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']}  style={styles.container}>
-        <Image source={require("../assets/Plate.png")} style={styles.image} />
-        <Text style={styles.title}>Enjoy Your <Text style={styles.title}>Meal</Text> </Text>
-        
-        <TouchableOpacity style={styles.btn}><Text style={styles.btnText}>Get Started</Text></TouchableOpacity>
-      </LinearGradient> 
+      <View  style={styles.container}>
+      <Text style={styles.title}>Hungry Hub</Text>
+      <LinearGradient style={styles.content} colors={['pink','purple']}>
+      <Image style={styles.landingbg} source={require('../assets/LandingBG1.png')} resizeMode='contain'></Image>
+      <View style={styles.about}>
+        <Text style={styles.titleContent}>Great Food</Text>
+        <Text style={styles.titleContent}>Is</Text>
+        <Text style={styles.titleContent}>Great Mood</Text>
+      </View>
+     <TouchableOpacity style={styles.btn}><Text style={styles.btnText}>Get Started</Text></TouchableOpacity>
+      </LinearGradient>
+       </View> 
     )
   }
 }
@@ -19,32 +25,57 @@ const styles = StyleSheet.create({
         display:'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        justifyContent:'space-evenly',
         alignItems: 'center',
-        backgroundColor:'#4b319f',
-        height:'100%',
+        backgroundColor:'#feedfc',
+        height:'100%'
+    },
+    content:{
+      backgroundColor:'pink',
+      height:'50%',
+      width:'80%',
+      padding:20,
+      borderRadius:60,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems:'center',
+      marginTop:'50%'
+    },
+    landingbg:{
+      height:'60%',
+      width:'100%',
+      position:'absolute',
+      top:'-30%'
     },
     title:{
+      fontSize:40,
+      fontWeight:'bold'
+    },
+    about:{
         display:'flex',
-        flexDirection: 'column',
-        fontSize:40,
-        color:'white',
-        fontWeight:'bold',
-        width:'50%'
+        justifyContent:'flex-end',
+        alignItems:'center',
+        marginTop:100,
+    },
+    titleContent:{
+      fontSize:40,
+      color:'white',
+      fontWeight:'bold',
     },
     btn:{
-        backgroundColor:'white',
+        backgroundColor:'#853dc2',
         borderRadius: 10,
         height:50,
         width:'40%',
         fontWeight: 'bold',
         alignItems:'center',
         justifyContent:'center',
-        padding:5
+        padding:5,
+        position:'absolute',
+        bottom:'-5%'
     },
     btnText:{
         fontWeight:'bold',
         fontSize:19,
-        color:'#36454F'
+        color:'white'
     }
 })
