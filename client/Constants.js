1ee2a1b40491233ba79df654c6ab0c8f5f33dd4b
AshutoshRath1612 <ashutosh.rath1613@ibm.com>
2024-03-24 23:17:58 +0530

    added constant file

---
+++ client/Constants.js
@@ -0,0 +1,6 @@
+export const Host = 'http://localhost:5000/'
+
+export const LoginRoute = 'auth/login'
+
+export const StudentRegisterRoute = 'auth/student/register'
+export const VendorRegisterRoute = 'auth/vendor/register'
