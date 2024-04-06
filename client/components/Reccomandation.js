311187a81eed4b00b9d1a9787f2655f337c883c9
AshutoshRath1612 <ashutosh.rath1613@ibm.com>
2024-04-06 14:13:08 +0530

    homepage

--- client/components/Reccomandation.js
+++ client/components/Reccomandation.js
@@ -8,6 +8,7 @@ import {
   Image,
 } from "react-native";
 import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function Reccomandation() {
   const data = [
@@ -16,44 +17,61 @@ export default function Reccomandation() {
       shopName: "Mio Amore",
       name: "Roll",
       price: 100,
+      type:'Vegeterian'
     },
     {
       image: require("../assets/Plate.png"),
       shopName: "Urban Flavours",
       name: "Chicken Tikka Masala",
       price: 1000,
+      type:'Non-Vegeterian'
     },
     {
       image: require("../assets/Plate.png"),
       shopName: "def",
       name: "Card 3",
       price: 1200,
+      type:'Non-Vegeterian'
     },
     {
       image: require("../assets/Plate.png"),
       shopName: "abc",
       name: "Card 4",
       price: 150,
+      type:'Vegeterian'
     },
     {
       image: require("../assets/Plate.png"),
       shopName: "abc",
       name: "Card 5",
       price: 130,
+      type:'Vegeterian'
     },
   ];
 
   const CardItem = ({ item }) => {
     return (
-      <View style={styles.card}>
+      <View style={[styles.card,  item.type === 'Vegeterian' ? {backgroundColor:'#127311'} : {backgroundColor:'#D31911'}]}>
           <Image source={item.image} resizeMode="contain" style={styles.itemImg} /> 
+          <View style={styles.cardInfo}>
+          <View style={styles.leftInfo}>
+            <Text style={styles.text}>{item.name.length >14 ? item.name.substring(0,14) + '...': item.name}</Text>
+            <Text style={[styles.text , {fontWeight:'bold'}]}>{item.shopName.length >14 ? item.shopName.substring(0,14) + '...': item.shopName}</Text>
+          </View>
+          <View style={styles.rightInfo}>
+            <Text style={styles.text}>₹ {item.price}</Text>
+            <Pressable style={styles.btn}>
+              <Text style={{color:'white',fontSize:13}}>Add <FontAwesome  color="white" name="plus"></FontAwesome></Text>
+            </Pressable>
+          </View>
+          </View>
       </View>
     );
   };
 
   return (
     <View style={styles.reccomandation}>
-      <Text style={styles.title}>Recommended for you</Text>
+      <Text style={styles.title}>Recommended for You</Text>
       <FlatList
         horizontal
         keyboardShouldPersistTaps="handled"
@@ -72,13 +90,13 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
   },
   card: {
-    width: 170,
+    width: 190,
     height: 150,
-    backgroundColor: "lightgrey",
     margin: 10,
-    justifyContent: "space-around",
+    justifyContent: "flex-start",
     alignItems: "center",
     borderRadius: 10,
+    elevation: 6, 
   },
   title: {
     fontSize: 25,
@@ -88,9 +106,42 @@ const styles = StyleSheet.create({
   },
   itemImg: {
     width: "100%",
-    height: "100%",
+    height: "60%",
     opacity: 0.8,
     justifyContent: "space-between",
-    alignItems: "center", // Set opacity to make the image slightly opaque
+    alignItems: "center",
+    borderRadius:10
   },
+  cardInfo:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal:2,
+    width:'100%',
+    height:'40%',
+  },
+  leftInfo:{
+    justifyContent: "space-around",
+    width:'65%',
+    paddingHorizontal:5,
+    alignItems:'flex-start'
+  },
+  rightInfo:{
+    justifyContent: "space-around",
+    width:'35%',
+    paddingHorizontal:5,
+    alignItems:'flex-end',
+  },
+  text:{
+    fontSize: 15,
+    color:'white',
+    fontFamily: "Ubuntu_400Regular",
+  },
+  btn:{
+    backgroundColor:'rgba(128,128,128,0.6)',
+    padding:4,
+    marginVertical:2,
+    borderRadius:5,
+    width:'100%',
+    alignItems:'center',
+  }
 });
--- client/components/ShopList.js
+++ client/components/ShopList.js
@@ -1,57 +1,195 @@
-import { View, Text, ScrollView, FlatList, StyleSheet } from 'react-native'
-import React from 'react'
-import Reccomandation from './Reccomandation';
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, StyleSheet, Image, FlatList, Dimensions } from "react-native";
+import TextSize from "react-native-text-size";
+import { FontAwesome } from "@expo/vector-icons";
 
+const averageCharacterWidth = 8;
 
 export default function ShopList() {
-        
-const DATA = [
-    { id: '1', title: 'Card 1' },
-    { id: '2', title: 'Card 2' },
-    { id: '3', title: 'Card 3' },
-    { id: '4', title: 'Card 4' },
-    { id: '5', title: 'Card 5' },
+  const DATA = [
+    {
+      shopName: "Roll Shop",
+      rating: 2.1,
+      ratingCount: 200,
+      food: [{ name: "Roll 1" }, { name: "Roll 2" }, { name: "Roll 3" }],
+    },
+    {
+      shopName: "Burger King",
+      rating: 4.3,
+      ratingCount: 300,
+      food: [{ name: "Burger 1" }, { name: "Burger 2" }, { name: "Burger 3" }],
+    },
+    {
+      shopName: "Halka Fhulka",
+      rating: 4.5,
+      ratingCount: 400,
+      food: [
+        { name: "Pani Puri" },
+        { name: "Dahi Puri" },
+        { name: "Sev Puri" },
+      ],
+    },
+    {
+      shopName: "Urban Flavour",
+      rating: 1,
+      ratingCount: 500,
+      food: [
+        { name: "Chicken Tikka Masala" },
+        { name: "Chicken Biryani" },
+        { name: "Chicken Korma" },
+      ],
+    },
+    {
+      shopName: "Maggi Center",
+      rating: 2.5,
+      ratingCount: 100,
+      food: [{ name: "Maggi" }, { name: "Maggi" }, { name: "Maggi" }],
+    },
+    {
+      shopName: "Mio Amore",
+      rating: 3.8,
+      ratingCount: 600,
+      food: [{ name: "Roll" }, { name: "Roll" }, { name: "Roll" }],
+    },
   ];
 
-  const CardItem = ({title}) =>{
+  const averageCharacterWidth = 8; // Assumed average character width in pixels
+
+const CardItem = ({ item }) => {
+  const [visibleFoodItems, setVisibleFoodItems] = useState([]);
+
+  useEffect(() => {
+    if(visibleFoodItems.length<15){
+      let foodItem =''
+      item.food.forEach((food) => {
+        for (let i = 0; i < food.name.length; i++) {
+          if (foodItem.length < 20) {
+            foodItem += food.name[i];
+          } else {
+            if(foodItem[foodItem.length-1] === ' ')
+              foodItem += '...';
+            break;
+          }
+        }
+        foodItem += ',';
+
+      })
+      foodItem[foodItem.length-1] = ''
+      foodItem += '...'
+      while(true){
+        if(foodItem[foodItem.lastIndexOf('...')-1] === ','){
+          foodItem = foodItem.substring(0,foodItem.lastIndexOf('...')-1) + "..."
+          console.log(foodItem)
+      }
+      else{
+        break
+      }
+    }
+      setVisibleFoodItems(foodItem)
+    }
+  }, []);
+  
     return (
-        <View style={styles.card}>
-        <Text>{title}</Text>
+      <View style={styles.card}>
+        <Image
+          style={styles.icon}
+          source={require("../assets/Plate.png")}
+        ></Image>
+        <View style={styles.info}>
+          <Text style={styles.shopNames}>{item.shopName}</Text>
+          <View style={styles.ratingContainer}>
+            <FontAwesome
+              name="star"
+              size={15}
+              color={item.rating > 4 ? "#006400" : item.rating>3 ? '#90EE90' : item.rating>2 ? '#FAD5A5' : "#FF6347"}
+            />
+            <Text style={styles.ratings}>{item.rating}</Text>
+            <Text>({item.ratingCount})</Text>
+          </View>
+          <View style={styles.foodContainer}>
+            <Text>
+
+            {visibleFoodItems}
+            </Text>
+          </View>
+        </View>
       </View>
-    )
-  }
+    );
+  };
+  
+  
   return (
     <View style={styles.container}>
-    <Text style={styles.title}>Shops</Text>
-      <FlatList 
+      <Text style={styles.title}>Shops</Text>
+      <FlatList
         data={DATA}
-        renderItem={({ item }) => <CardItem title={item.title} />}
+        renderItem={({ item }) => <CardItem item={item} />}
         showsVerticalScrollIndicator={false}
-         keyExtractor={item => item.id}
+        keyExtractor={(item, index) => index}
       />
-
     </View>
-  )
+  );
 }
 const styles = StyleSheet.create({
-  container:{
-      alignItems:'flex-start',
-      paddingHorizontal:10,
-      marginBottom:0,
-      flex:1,
-    },
-    card: {
-      width: 350,
-      height: 150,
-      backgroundColor: 'lightblue',
-      margin: 10,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius:20
+  container: {
+    alignItems: "flex-start",
+    paddingHorizontal: 10,
+    marginBottom: 0,
+    flex: 1,
   },
-  title:{
-      fontSize:25,
-      marginLeft:10,
-      fontFamily:'Ubuntu_700Bold',
-  }
-})
+  title: {
+    fontSize: 25,
+    marginLeft: 10,
+    fontFamily: "Ubuntu_700Bold",
+  },
+  card: {
+    width: 350,
+    height: 150,
+    backgroundColor: "white",
+    margin: 10,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: 20,
+    flexDirection: "row",
+    shadowOffset: { width: 4, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 6,
+    elevation: 8,
+  },
+  icon: {
+    height: 150,
+    width: 150,
+    resizeMode: "contain",
+    width: "40%",
+  },
+  info: {
+    justifyContent: "space-around",
+    alignItems: "flex-start",
+    width: "60%",
+  },
+  shopNames: {
+    fontSize: 25,
+    color: "black",
+    fontFamily: "Ubuntu_700Bold",
+  },
+  ratings: {
+    fontSize: 16,
+    color: "grey",
+  },
+  ratingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: "50%",
+  },
+  foodContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    width: "100%",
+    paddingTop: 10,
+  },
+  foodItem: {
+    marginRight: 5,
+  },
+});
