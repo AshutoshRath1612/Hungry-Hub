8358e9637fd2938b3d15221a32989f01139f6c84
Ashutosh Rath <83402676+AshutoshRath1612@users.noreply.github.com>
2024-05-17 16:13:16 +0530

    Update payment.py

--- server1/payment.py
+++ server1/payment.py
@@ -1,6 +1,6 @@
 import razorpay
 
-razorpay_client = razorpay.Client(auth=("rzp_test_1iMLg2obI6ZsN0", "lklp2qnIOF9DlXYYMRI7FjTf"))
+razorpay_client = razorpay.Client(auth=())
 
 def create_order(amount):
     currency = "INR"  # Change to your currency if different
